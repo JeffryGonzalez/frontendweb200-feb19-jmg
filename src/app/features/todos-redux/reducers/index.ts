5935c457eebde30b0553cbf0d2e739befe4c1fab
Jeffry Gonzalez <jeff@hypertheory.com>
2019-02-13 13:20:17 -0500

    message added

--- src/app/features/todos-redux/reducers/index.ts
+++ src/app/features/todos-redux/reducers/index.ts
@@ -1,5 +1,5 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromMessage from './messages.reducer';
-
 export interface TodosState {
   message: fromMessage.State;
 }
@@ -7,3 +7,14 @@ export interface TodosState {
 export const reducers = {
   message: fromMessage.reducer
 };
+
+// 1. Create A Feature Selector
+const selectTodosFeature = createFeatureSelector<TodosState>('todosFeature');
+// 2. Create a Selector for Each Branch of the Feature
+const selectMessage = createSelector(selectTodosFeature, f => f.message);
+// 3. Create any "helpers" you might need (optional)
+
+// 4. Create a selector for what the component needs.
+
+// selectHeading: string
+export const selectHeaderMessage = createSelector(selectMessage, m => m.heading);
